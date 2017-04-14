472f6715c47af1435677d507a0fa0368b02fd50c
cuong0993 <cuong0993@gmail.com>
2017-04-14 10:33:34 +0700

    Reformat

--- src/main/java/net/astah/plugin/yuml/draw/Direction.java
+++ src/main/java/net/astah/plugin/yuml/draw/Direction.java
@@ -1,16 +1,16 @@
 package net.astah.plugin.yuml.draw;
 
-public enum Direction{
-        LEFT_TO_RIGHT("LR"),TOP_DOWN("TB"),RIGHT_TO_LEFT("RL");
+public enum Direction {
+    LEFT_TO_RIGHT("LR"), TOP_DOWN("TB"), RIGHT_TO_LEFT("RL");
 
-private String text;
+    private String text;
 
-        Direction(String text){
-        this.text=text;
-        }
+    Direction(String text) {
+        this.text = text;
+    }
 
-@Override
-public String toString(){
+    @Override
+    public String toString() {
         return text;
-        }
-        }
+    }
+}
